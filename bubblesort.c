36ee24ff3d8d14a528a8ba14aa9018709c0d47d8
Musaib shakeel <hajimusaib0@gmail.com>
2026-01-27 17:09:16 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void display(int arr[],int size){
-        for(int i=0;i<size;i++){
+        for(int i=0;i<size;i++)
+        {
             printf("%d ",arr[i]);
         }
         printf("\n");
@@ -24,7 +25,8 @@ int main (){
         printf("Enter The array size: ");
         scanf("%d",&size);
             printf("Enter The Array Elements: ");
-            for(int i=0;i<size;i++){
+            for(int i=0;i<size;i++)
+            {
                 scanf("%d",&arr[i]);
             }
             printf("Before Sorting\n");
